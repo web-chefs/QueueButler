29b7efcceb2298e92d8fd11d26073408755a4365
Justin Fossey <justinfossey@gmail.com>
2017-09-06 17:54:13 +0200

    Add Laravel 5.4 support

---
+++ src/Versions/Laravel5_4BatchCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WebChefs\QueueButler\Versions;
+
+// Package
+use Illuminate\Queue\WorkerOptions;
+use WebChefs\QueueButler\BatchRunner;
+
+class Laravel5_4BatchCommand extends BatchRunner
+{
+    // 5.4 does not require modifications
+}
