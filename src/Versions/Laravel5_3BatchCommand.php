693154a95c7b250d6652a933fed47791ade046c3
Justin Fossey <justinfossey@gmail.com>
2017-09-06 17:54:05 +0200

    Add Laravel 5.3 support

---
+++ src/Versions/Laravel5_3BatchCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace WebChefs\QueueButler\Versions;
+
+// Package
+use Illuminate\Queue\WorkerOptions;
+use WebChefs\QueueButler\BatchRunner;
+
+class Laravel5_3BatchCommand extends BatchRunner
+{
+
+    /**
+     * Listen to the given queue in a loop.
+     *
+     * @param  string  $connectionName
+     * @param  string  $queue
+     * @param  \Illuminate\Queue\WorkerOptions  $options
+     * @return void
+     */
+    public function daemon($connectionName, $queue, WorkerOptions $options)
+    {
+        $lastRestart = $this->getTimestampOfLastQueueRestart();
+        while (true) {
+            $this->registerTimeoutHandler($options);
+            if ($this->daemonShouldRun($options)) {
+                $this->runNextJob($connectionName, $queue, $options);
+            } else {
+                $this->sleep($options->sleep);
+            }
+            if ($this->memoryExceeded($options->memory) ||
+                $this->queueShouldRestart($lastRestart)) {
+                $this->stop();
+            }
+
+            // Inject our limit check
+            $this->checkLimits($options);
+        }
+    }
+
+}
