e6cbf76fe5d5e4d7e7ed5ddf4a90f75874cea027
Justin Fossey <justinfossey@gmail.com>
2017-09-07 10:31:38 +0200

    Add Laravel 5.5 support

---
+++ src/Versions/Laravel5_5BatchCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WebChefs\QueueButler\Versions;
+
+// Package
+use Illuminate\Queue\WorkerOptions;
+use WebChefs\QueueButler\BatchRunner;
+
+class Laravel5_5BatchCommand extends BatchRunner
+{
+    // 5.5 does not require modifications
+}
