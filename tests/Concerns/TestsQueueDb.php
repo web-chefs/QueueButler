4baf9af79b4e2e038db36a03855aa35df0941b4a
Justin Fossey <justinfossey@gmail.com>
2017-09-13 18:07:09 +0200

    Start trying to test jobs using batch

---
+++ tests/Concerns/TestsQueueDb.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace WebChefs\QueueButler\Tests\Concerns;
+
+// Aliases
+use DB;
+
+trait TestsQueueDb
+{
+
+    /**
+     * Our test database connection name.
+     *
+     * @return string
+     */
+    protected function queueTestDbName()
+    {
+        return 'sqlite_mem_testing';
+    }
+
+    /**
+     * Build a test Db to query to jobs table.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    protected function queueTestDbQuery()
+    {
+        return DB::connection($this->queueTestDbName())->table('jobs');
+    }
+
+    /**
+     * Set default database ENV to be our in memory sqlite db.
+     *
+     * @return void
+     */
+    protected function runTestQueueDbEnv()
+    {
+        // Set test db as default environment
+        putenv('DB_DEFAULT=' . $this->queueTestDbName());
+    }
+
+    /**
+     * Setup job queues to run form a in memory sqlite database.
+     *
+     * @return void
+     */
+    protected function runTestQueueDb()
+    {
+        // Modify config
+        $config = $this->app->make('config');
+        $this->queueTestDb_dbConfig($config);
+        $this->queueTestDb_queueConfig($config);
+
+        // Run our migrations
+        $dbPath = $this->app->databasePath();
+        $this->app->useDatabasePath($this->testEnvPath);
+        $this->artisan('migrate:refresh');
+        $this->app->useDatabasePath($dbPath);
+    }
+
+    /**
+     * Set the database connection to a in memory sqlite database.
+     *
+     * @param \Illuminate\Config\Repository $config
+     */
+    protected function queueTestDb_dbConfig($config)
+    {
+        // Setup test DB
+        $config->set('database.connections.' . $this->queueTestDbName(), [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+        $config->set('database.default', $this->queueTestDbName());
+    }
+
+    /**
+     * Set the queue driver to be our database.
+     *
+     * @param \Illuminate\Config\Repository $config
+     */
+    protected function queueTestDb_queueConfig($config)
+    {
+        $config->set('queue.connections.' . $this->queueTestDbName(), [
+            'driver'      => 'database',
+            'table'       => 'jobs',
+            'queue'       => 'default',
+            'retry_after' => 90,
+        ]);
+        $config->set('queue.default', $this->queueTestDbName());
+    }
+
+}
---
+++ tests/Jobs/QueueBatchTestJob.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace  WebChefs\QueueButler\Tests\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class QueueBatchTestJob implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        app()->bind('QueueBatchRunTestAnswer', function($app) {
+            return $app->make('QueueBatchRunAnswerToken');
+        });
+    }
+}
