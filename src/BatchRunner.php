b56e77bc1aa09e81b16cb1de11197e5f086c078a
Justin Fossey <justinfossey@gmail.com>
2017-10-13 18:07:53 +0200

    Updated BatchRunner to use a Stop Exception to gracefully exit rather than killing the processes and improve limit checking

--- src/BatchRunner.php
+++ src/BatchRunner.php
@@ -4,6 +4,7 @@ namespace WebChefs\QueueButler;
 
 // Package
 use WebChefs\QueueButler\BatchOptions;
+use WebChefs\QueueButler\Exceptions\StopBatch;
 
 // Framework
 use Illuminate\Queue\Worker;
@@ -32,25 +33,18 @@ class BatchRunner extends Worker
      */
     public function batch($connectionName, $queue, BatchOptions $options)
     {
+        $this->options   = $options;
         $this->startTime = microtime(true);
         $this->jobCount  = 0;
-        parent::daemon($connectionName, $queue, $options);
-    }
 
-    /**
-     * Listen to the given queue in a loop.
-     *
-     * @param  string  $connectionName
-     * @param  string  $queue
-     * @param  \Illuminate\Queue\WorkerOptions  $options
-     * @return void
-     */
-    public function daemon($connectionName, $queue, WorkerOptions $options)
-    {
-        $this->validOptions($options);
-
-        // For daemon to use batch if called by accident.
-        $this->batch($connectionName, $queue, $options);
+        try {
+           $this->daemon($connectionName, $queue, $options);
+        }
+        catch (StopBatch $excption) {
+            // Check if the batch was cleanly stopped
+            // The batch hit a limit
+            return;
+        }
     }
 
     /**
@@ -70,6 +64,31 @@ class BatchRunner extends Worker
     }
 
     /**
+     * Determine if the batch should process on this iteration.
+     *
+     * @return bool
+     */
+    protected function daemonShouldRun(WorkerOptions $options)
+    {
+        $this->checkLimits();
+        return parent::daemonShouldRun($options);
+    }
+
+    /**
+     * Raise the after queue job event.
+     *
+     * @param  string  $connectionName
+     * @param  \Illuminate\Contracts\Queue\Job  $job
+     * @return void
+     */
+    protected function raiseAfterJobEvent($connectionName, $job)
+    {
+        $this->jobCount++;
+        parent::raiseAfterJobEvent($connectionName, $job);
+        $this->checkLimits();
+    }
+
+    /**
      * Stop the process if necessary.
      *
      * @param  WorkerOptions  $options
@@ -77,13 +96,43 @@ class BatchRunner extends Worker
      */
     protected function stopIfNecessary(WorkerOptions $options, $lastRestart)
     {
-        $this->validOptions($options);
+        parent::stopIfNecessary($options, $lastRestart);
+        $this->checkLimits();
+    }
 
-        if ($this->isTimeLimit($options->timeLimit) || $this->isJobLimit($options->jobLimit)) {
+    /**
+     * Sleep the script for a given number of seconds.
+     *
+     * @param  int   $seconds
+     * @return void
+     */
+    public function sleep($seconds)
+    {
+        $this->checkLimits();
+        parent::sleep($seconds);
+    }
+
+    /**
+     * Stop listening and bail out of the script.
+     *
+     * @return void
+     */
+    public function stop($status = 0)
+    {
+        // Cleanly handle stopping a batch without resorting to killing the process
+        throw new StopBatch();
+    }
+
+    /**
+     * Check our batch limits and stop the command if we reach a limit.
+     *
+     * @param  WorkerOptions $options
+     */
+    protected function checkLimits()
+    {
+        if ($this->isTimeLimit($this->options->timeLimit) || $this->isJobLimit($this->options->jobLimit)) {
             $this->stop();
         }
-
-        parent::stopIfNecessary($options, $lastRestart);
     }
 
     /**
